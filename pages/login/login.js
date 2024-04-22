38d1d0edceaa3ebcd477245793f1ce793580d980
wanhuaide <1249185261@qq.com>
2024-04-22 23:38:59 +0800

    登录界面的跳转

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,11 +1,54 @@
 // pages/login/login.js
 Page({
+  searchUserName: function (e) {
+      this.setData({
+          username: e.detail.value
+      })
+  },
+  searchPassWord: function (e) {
+      this.setData({
+          password: e.detail.value
+      })
+  },
+  //登录
+  go: function () {
+      if (this.data.username == '' || this.data.password == '') {
+          wx.showToast({
+              title: '不能为空',
+              icon: 'error',
+              duration: 1000 //持续的时间
+          })
+      } else if (this.data.username != 'admin' || this.data.password != 'admin') {
+          wx.showToast({
+              title: '用户名密码错误',
+              icon: 'error',
+              duration: 1000 //持续的时间
+          })
+      } else {
+          wx.switchTab({
+              url: "/pages/index/index"
+          })
+      }
+  },
+
+  //跳转到刷脸登录
+  goBrushFace:function() {
+      wx.navigateTo({
+          url: "/pages/brushface/brushface"
+      })
+  },
+  goOneClickLogin:function() {
+      wx.navigateTo({
+          url: "/pages/oneclicklogin/oneclicklogin"
+      })
+  },
 
   /**
    * 页面的初始数据
    */
   data: {
-
+      username: '',
+      password: ''
   },
 
   /**
