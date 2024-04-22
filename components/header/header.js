94988e237f645fd793bc1a5d483c1a8da4a42823
wanhuaide <1249185261@qq.com>
2024-04-22 21:05:30 +0800

    底边导航

---
+++ components/header/header.js
@@ -0,0 +1,24 @@
+// components/header/header.js
+Component({
+
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        paddingTopNum: wx.getSystemInfoSync().statusBarHeight
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
